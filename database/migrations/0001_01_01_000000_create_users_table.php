941159c65037ae2a9b2d3ed6768280754b793081
DinGo21 <diego.antoniosantamaria@gmail.com>
2025-03-18 17:28:59 +0100

    fix: database migration

--- database/migrations/0001_01_01_000000_create_users_table.php
+++ database/migrations/0001_01_01_000000_create_users_table.php
@@ -19,7 +19,7 @@ return new class extends Migration
             $table->string('password');
             $table->rememberToken();
             $table->timestamps();
-            $table->string('location');
+            $table->string('location')->default("none");
         });
 
         Schema::create('password_reset_tokens', function (Blueprint $table) {
