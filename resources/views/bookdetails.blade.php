c6f122e076a1ddae21e6fb8aeb858dc630f87d60
Ayuik <marcosayelen@gmail.com>
2025-03-20 11:47:32 +0100

    feat: blade created and route connected

---
+++ resources/views/bookdetails.blade.php
@@ -0,0 +1,5 @@
+@extends('layouts.app2')
+@section('bookdetails')
+<div id="bookdetails">
+</div>
+@endsection
--- routes/web.php
+++ routes/web.php
@@ -5,3 +5,7 @@ use Illuminate\Support\Facades\Route;
 Auth::routes();
 
 Route::get('/', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
+
+Route::get('/bookdetails', function() {
+    return view('bookdetails');
+})->name('bookdetails');
