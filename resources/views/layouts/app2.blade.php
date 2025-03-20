656a70b88e2a4674b1463131d320144dd5f08f14
TOKIO-V2 <albertohernandezgaleote6@gmail.com>
2025-03-20 21:25:14 +0100

    fix: Footer team Naming and github icon.

--- resources/views/layouts/app2.blade.php
+++ resources/views/layouts/app2.blade.php
@@ -80,7 +80,7 @@
         <main class="py-4">
             @yield('content')
         </main>
-        <footer class="d-flex flex-wrap justify-content-between align-items-center py-3 my-4 border-top">
+        <footer class="d-flex flex-wrap justify-content-between align-items-center py-3 my-4 border-top ">
             <div class="col-md-4 d-flex align-items-center">
                 <a href="/" class="mb-3 me-2 mb-md-0 text-body-secondary text-decoration-none lh-1">
                     <svg class="bi" width="30" height="24"><use xlink:href="#bootstrap"></use></svg>
@@ -89,7 +89,7 @@
             </div>
             
             <ul class="nav col-md-4 justify-content-end list-unstyled d-flex">
-                <li class="ms-3"><a href="https://github.com/Equipo-3-Hackaton-Globant/ReadersMatch">Equipo 3 <i class="bi bi-github"></i></a></li>
+                <li class="ms-3"><a href="https://github.com/Equipo-3-Hackaton-Globant/ReadersMatch"> <img src="{{asset("images/githubIMG.png")}}" height="50px" class='gitHubImg'> ReadersMatch Team <i class="bi bi-github"></i></a></li>
             </ul>
         </footer>
     </div>
