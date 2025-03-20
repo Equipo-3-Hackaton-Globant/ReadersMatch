3bc00554530bd7179294308e77d25c3c3245de02
catmaluci <catmaluci@hotmail.com>
2025-03-20 14:13:15 +0100

    calling database to display books in homepage

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -1,14 +1,10 @@
 @extends('layouts.app')
 
 @section('content')
-    <!-- <div id="login">
-
-        </div> -->
     <div class="container">
-
         <div class="row justify-content-center">
             <div class="col-md-8">
-                <h3> Hola,{{ Auth::user()->name }} </h3>
+                <h3> Hola, {{ Auth::user()->name }} </h3>
                 <h1> Encuentra tu Libro </h1>
             </div>
         </div>
@@ -21,34 +17,30 @@
             </div>
         </div>
         <div class="row justify-content-center">
-            <div class="col-md-8 py-4">
+            <div class="col-md-12 py-4">
                 <h3> Ultimos Publicados </h3>
-                <div>
-                @foreach($books as $book)
-                    <div class="card" style="width: 100vh;">
-                        <img class="card-img-top" src="{{ asset($book->imgURL) }}" alt="Card image cap">
-                        <div class="card-body">
-                            <h5 class="card-title">{{$book->title}}</h5>
-                            <p class="card-text">{{$book->author}}</p>
-                            <a href="{{route('bookdetails')}}" class="btn btn-primary">{{$book->observation}}</a>
+                <div class="row">
+                    @foreach($books as $book)
+                        <div class="col-md-3 mb-4">
+                            <div class="card">
+                                <img class="card-img-top" src="{{ asset($book->imgURL) }}" alt="Card image cap">
+                                <div class="card-body">
+                                    <h5 class="card-title">{{$book->title}}</h5>
+                                    <p class="card-text">{{$book->author}}</p>
+                                    <a href="{{route('bookdetails')}}" class="btn btn-primary">{{'Detalles'}}</a>
+                                </div>
+                            </div>
                         </div>
-                    </div>
-                @endforeach
+                    @endforeach
+                </div>
             </div>
         </div>
-    </div>
-
-    <div class="card-body">
-        @if (session('status'))
-            <div class="alert alert-success" role="alert">
-                {{ session('status') }}
-            </div>
-        @endif
-
-       
-    </div>
-    </div>
-    </div>
-    </div>
+        <div class="card-body">
+            @if (session('status'))
+                <div class="alert alert-success" role="alert">
+                    {{ session('status') }}
+                </div>
+            @endif
+        </div>
     </div>
 @endsection
